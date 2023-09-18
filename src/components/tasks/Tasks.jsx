737adf0f14bdeed723de95c349a7b4dc777cdfb3
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-18 13:51:15 +0200

    Feat: Create basic table structure + install MUI packages

--- src/components/tasks/Tasks.jsx
+++ src/components/tasks/Tasks.jsx
@@ -2,5 +2,19 @@ import React from "react";
 import styles from "./Tasks.module.css";
 
 export default function Tasks() {
-  return <div>Tasks</div>;
+  return (
+    <div className={styles.tableContainer}>
+      <table>
+        <tr className="topRow">
+          <th>My checklist</th>
+        </tr>
+        <tr className="middleRow">
+          <th>Title</th>
+        </tr>
+        <tr className="bottomRow">
+          <th>Completed</th>
+        </tr>
+      </table>
+    </div>
+  );
 }
