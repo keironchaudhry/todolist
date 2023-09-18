9bcccdbc78278396ba4a911a4789a304439b1e32
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-18 10:08:08 +0200

    Setup: Create + import initial component into home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Home() {
-  return <div>Hello world from home page</div>;
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
 }
