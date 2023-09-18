be404dbef255adba927a782d94d2ef96eee01d0c
twiggygnome <78651133+twiggygnome@users.noreply.github.com>
2023-09-18 10:19:36 +0200

    Feat: Create basic design for navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
 import React from "react";
+import styles from "./Navbar.module.css";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className={styles.navbarContainer}>
+      <div className={styles.logo}>Todo List</div>
+    </div>
+  );
 }
